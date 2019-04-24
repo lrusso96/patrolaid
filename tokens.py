8822a2dc47c2338204eff908a350a389b4d19bf7
Luigi <russo.1699981@studenti.uniroma1.it>
2019-04-24 11:27:56 +0200

    pydoc for tokens
    fixed issue #1

--- tokens.py
+++ tokens.py
@@ -2,6 +2,13 @@ from enum import Enum
 
 
 class Token(Enum):
+    """This class is an Enumeration for WikiMedia Tokens.
+    Most of them are self explaining.
+
+    History:
+        v1.27: 'login' and 'createaccount' were introduced.
+    """
+
     CREATE_ACCOUNT = 'createaccount'
     CSRF = 'csrf'
     DELETE_GLOBAL_ACCOUNT = 'deleteglobalaccount'
