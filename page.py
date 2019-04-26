6bd3ea357a3032bc7e38b5219b85bd8d2307852b
Luigi <russo.1699981@studenti.uniroma1.it>
2019-04-26 13:36:55 +0200

    simple models for page (#3) and revision (#4), get last revisions
    caution: no error handling at all

---
+++ page.py
@@ -0,0 +1,10 @@
+from revision import Revision
+
+
+class Page:
+    def __init__(self, **data):
+        self.page_id = data['pageid']
+        self.ns = data['ns']
+        self.title = data['title']
+        self.revisions = [Revision(self, **rev) for rev in data['revisions']]
+        self.last_rev_id = self.revisions[0].id # assume to get always the last revisions!
